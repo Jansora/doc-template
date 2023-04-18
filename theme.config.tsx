cf60250e48e215011b64ac8f05484d30e755044b
Jansora <zhangyue1936@gmail.com>
2023-04-18 18:18:20 +0800

    update doc

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-
+// import dayjs from 'm'
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>Jansora's Document</span>,
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Jansora\'s Document',
   },
+  toc: {
+    float: true,
+    title: "侧边目录栏"
+  },
+  feedback: {
+    content: null,
+  },
+  editLink: {
+    text: null
+  },
+  // gitTimestamp: <span> 上次更新: </span>,
+  // gitTimestamp: true
+  gitTimestamp: ({timestamp}) => <span> 上次更新: {timestamp.toLocaleString()} </span>
 }
 
 export default config
